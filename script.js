4c6251751c965250bf2ae0116a985b78d2473866
Emminet29 <139685068+Emminet29@users.noreply.github.com>
2024-01-24 21:01:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -147,8 +147,6 @@ circles.forEach((elem) => {
 
 // mix it up portfolio section
 
-var mixer = mixitup(".portfolio-gallery");
-
 // Active Menu ////////////////////////////////////
 
 let menuLi = document.querySelectorAll("header ul li a");
